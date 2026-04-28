ebd5336bc432ff289c850a43e336fa517084d0b8
Matheus G. Dias <matheusgiga123@gmail.com>
2026-04-28 14:39:04 -0300

    working with structs

---
+++ rectangles/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let scale = 1;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let rect4 = Rectangle::square(2);
+
+    println!("rect4: {rect4:#?}");
+
+    dbg!(&rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3: {}", rect1.can_hold(&rect3));
+}
